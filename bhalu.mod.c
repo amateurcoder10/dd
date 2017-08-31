98926b4e7f8382c479594194ea7df83545592d48
Prashanthi Kadambi <Prashanthi.S.K@iiitb.org>
2017-08-31 18:09:43 +0530

    Add files via upload

---
+++ bhalu.mod.c
@@ -0,0 +1,40 @@
+#include <linux/module.h>
+#include <linux/vermagic.h>
+#include <linux/compiler.h>
+
+MODULE_INFO(vermagic, VERMAGIC_STRING);
+
+__visible struct module __this_module
+__attribute__((section(".gnu.linkonce.this_module"))) = {
+	.name = KBUILD_MODNAME,
+	.init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+	.exit = cleanup_module,
+#endif
+	.arch = MODULE_ARCH_INIT,
+};
+
+static const struct modversion_info ____versions[]
+__used
+__attribute__((section("__versions"))) = {
+	{ 0xe89b6b83, __VMLINUX_SYMBOL_STR(module_layout) },
+	{ 0x8bbd41b6, __VMLINUX_SYMBOL_STR(param_ops_int) },
+	{ 0xd766d65e, __VMLINUX_SYMBOL_STR(device_unregister) },
+	{ 0x23aa8d3f, __VMLINUX_SYMBOL_STR(device_remove_file) },
+	{ 0x5a07e25b, __VMLINUX_SYMBOL_STR(device_register) },
+	{ 0x27e1a049, __VMLINUX_SYMBOL_STR(printk) },
+	{ 0x28318305, __VMLINUX_SYMBOL_STR(snprintf) },
+	{ 0xdb7305a1, __VMLINUX_SYMBOL_STR(__stack_chk_fail) },
+	{ 0xe2d5255a, __VMLINUX_SYMBOL_STR(strcmp) },
+	{ 0x20c55ae0, __VMLINUX_SYMBOL_STR(sscanf) },
+	{ 0x33b71906, __VMLINUX_SYMBOL_STR(dev_alert) },
+	{ 0xbdfb6dbb, __VMLINUX_SYMBOL_STR(__fentry__) },
+};
+
+static const char __module_depends[]
+__used
+__attribute__((section(".modinfo"))) =
+"depends=";
+
+
+MODULE_INFO(srcversion, "5124E28D83E5C1B300D5E98");
